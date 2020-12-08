08cdb133c4f27bc9db88976bd7c08fcdea9d7018
Bilal Sharqi <bilalsh@umich.edu>
2020-12-08 10:51:57 -0500

    changed version from 3.0.0 to 3.0.2
    This makes the tags 3.0.2 on both the original repo (https://github.com/chrislupp/MTK/tree/3.0.2) and the A2SRL fork the same.

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ for e in exts:
     e.cython_directives = {"embedsignature": True, "binding":True}
     
 setup(name='modaltoolkit',
-    version='3.0.0',
+    version='3.0.2',
     description='Modal Tool Kit',
     author='Christopher A. Lupp',
     author_email='clupp@umich.edu',
